9b80adfd9f854b3506fe6abfde420d0c4d121b92
JoabOwala <owalajoab@gmail.com>
2023-06-07 16:17:30 +0300

    seeding customers

--- kibandaski-server/db/seeds.rb
+++ kibandaski-server/db/seeds.rb
@@ -1,50 +1,59 @@
-puts "Clearing stale data..."
+    puts "Clearing stale data..."
 
-Owner.destroy_all
-Restaurant.destroy_all
-Menu.destroy_all
-Customer.destroy_all
-Order.destroy_all
+    Owner.destroy_all
+    Restaurant.destroy_all
+    Menu.destroy_all
+    Customer.destroy_all
+    Order.destroy_all
 
-puts "🌱 Seeding owners..."
+    puts "🌱 Seeding owners..."
 
-owner1 = Owner.create(name: 'Mary Wanjiku')
+    owner1 = Owner.create(name: 'Mary Wanjiku')
 
-owner2 = Owner.create(name: 'Jack Msee')
+    owner2 = Owner.create(name: 'Jack Msee')
 
-owner3 = Owner.create(name: 'Mama Ounde')
+    owner3 = Owner.create(name: 'Mama Ounde')
 
-owner4 = Owner.create(name: 'Jonte Were')
+    owner4 = Owner.create(name: 'Jonte Were')
 
-puts "🌱 Seeding restaurants..."
+   puts "🌱 Seeding restaurants..."
 
     restaurant1 = Restaurant.create(
-    name: 'KwaWanjiku Hotel', 
-    location: 'GuruNanak', 
-    opening_time: Time.new(0, 1, 1, 8, 0, 0),
-    closing_time: Time.new(0, 1, 1, 19, 0, 0)
+        owner: owner1
+        name: 'KwaWanjiku Hotel', 
+        location: 'GuruNanak', 
+        opening_time: Time.new(0, 1, 1, 8, 0, 0),
+        closing_time: Time.new(0, 1, 1, 19, 0, 0)
     )
 
     restaurant2 = Restaurant.create(
-    name: 'ChomaBase Hotel', 
-    location: 'Kyamichael', 
-    opening_time: Time.new(0, 1, 1, 8, 0, 0),
-    closing_time: Time.new(0, 1, 1, 19, 0, 0)
+        owner: owner2
+        name: 'ChomaBase Hotel', 
+        location: 'Kyamichael', 
+        opening_time: Time.new(0, 1, 1, 8, 0, 0),
+        closing_time: Time.new(0, 1, 1, 19, 0, 0)
     )
 
     restaurant3 = Restaurant.create(
-    name: 'Ounde Hotel', 
-    location: 'Githurai 45', 
-    opening_time: Time.new(0, 1, 1, 8, 0, 0),
-    closing_time: Time.new(0, 1, 1, 19, 0, 0)
+        owner: owner3
+        name: 'Ounde Hotel', 
+        location: 'Githurai 45', 
+        opening_time: Time.new(0, 1, 1, 8, 0, 0),
+        closing_time: Time.new(0, 1, 1, 19, 0, 0)
     )
 
     restaurant4 = Restaurant.create(
-    name: 'KwaJonte Hotel', 
-    location: 'Roysambu', 
-    opening_time: Time.new(0, 1, 1, 8, 0, 0),
-    closing_time: Time.new(0, 1, 1, 19, 0, 0)
+        owner: owner4
+        name: 'KwaJonte Hotel', 
+        location: 'Roysambu', 
+        opening_time: Time.new(0, 1, 1, 8, 0, 0),
+        closing_time: Time.new(0, 1, 1, 19, 0, 0)
     )
 
 
+    puts "🌱 Seeding customers..."
+    customer1 = Customer.create(name: 'Nick Muhwana')
+    customer2 = Customer.create(name: 'Gradus Pondi')
+    customer3 = Customer.create(name: 'Robert Muhaha')
+    customer4 = Customer.create(name: 'Jane Doe')
 puts "✅ Done seeding!"
