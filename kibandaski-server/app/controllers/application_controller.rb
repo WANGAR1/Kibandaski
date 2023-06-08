fb26c39ac8625c5b21570686c0c70289b85430e9
JoabOwala <owalajoab@gmail.com>
2023-06-08 15:00:52 +0300

    more specific paths

--- kibandaski-server/app/controllers/application_controller.rb
+++ kibandaski-server/app/controllers/application_controller.rb
@@ -2,10 +2,29 @@ class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
 
     #Add routes
+    #Get all owners
     get '/owners' do
         owners = Owner.all 
         owners.to_json
     end
+
+    #find menu by resturant
+    get '/menus/:id' do 
+        menus = Menu.find(params[:id])
+        menus.to_json
+    end
+
+    #edit menu 
+    patch '/menus/:id' do 
+        menus = Menu.find(params[:id])
+        menus.update(breakfast: params[:breakfast])
+        menus.update(lunch: params[:lunch])
+        menus.update(dinner: params[:dinner])
+        menus.to_json
+    end
+    
+    #menu edit by restaurant owner
+
 
     get '/customers' do 
         customers = Customer.all
